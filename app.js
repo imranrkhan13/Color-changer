473d3873a5982ee6ebb05ad7025eab99acb90d95
Imran Khan <99342205+imranrkhan13@users.noreply.github.com>
2023-11-06 20:37:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+let previousRandomNumber;
+
+btn.addEventListener('click', function() {
+    let randomNumber;
+    
+    do {
+        randomNumber = getRandomNumber();
+    } while (randomNumber === previousRandomNumber);
+
+    previousRandomNumber = randomNumber;
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
